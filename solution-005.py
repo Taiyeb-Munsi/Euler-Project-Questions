6eec08adc7c0cb94cdfe13f8e96154b4d83ccd6a
Taiyeb Munsi <taiyebmunsi@gmail.com>
2026-05-14 19:53:04 +0530

    Done : Problem 5

---
+++ solution-005.py
@@ -0,0 +1,21 @@
+import math
+
+def prime(n):
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n%i == 0:
+            return False 
+
+    return True
+
+result = 1
+power = 1
+
+for i in range(2,20):
+    if prime(i):
+        for j in range(1,20):
+            if i**j < 20:
+                power = i**j
+
+        result *= power
+
+print(result)
